5c5ada4c0c800c71738a2fdeea08edc92d6a4dcf
rseeda722 <ravikumar_seedarla@cable.comcast.com>
2021-10-24 21:30:02 -0400

    testing pre commit

---
+++ src/main/java/com/learning/work/com/learningMedia/first/FirstClass.java
@@ -0,0 +1,11 @@
+package com.learning.work.com.learningMedia.first;
+
+public class FirstClass {
+
+	public static void first() {
+		System.out.println("first Test");
+		System.out.println("first Test 2");
+		System.out.println("first Test 3");
+	}
+
+}
