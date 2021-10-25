097b079520e9bfcc2a6774e1597331b50881a902
rseeda722 <ravikumar_seedarla@cable.comcast.com>
2021-10-24 20:50:23 -0400

    test

---
+++ src/main/java/com/learning/work/com/learningMedia/LearningMediaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.work.com.learningMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/work/com/learningMedia/LearningMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.work.com.learningMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
